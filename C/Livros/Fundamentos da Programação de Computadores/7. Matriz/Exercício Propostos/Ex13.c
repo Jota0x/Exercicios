d5a477dc41eda0d284a3a618ea1f8bdb2bf5f929
Joao <joaopedrocamp554@gmail.com>
2025-10-30 23:50:59 -0300

    rever 11 e 12

---
+++ C/Livros/Fundamentos da Programação de Computadores/7. Matriz/Exercício Propostos/Ex13.c
@@ -0,0 +1,15 @@
+/*
+Elabore um programa que: preencha uma matriz 6  4; recalcule a matriz digitada, onde cada linha de-
+verá ser multiplicada pelo maior elemento da linha em questão; mostre a matriz resultante.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+
+    return 0;
+}//end main
