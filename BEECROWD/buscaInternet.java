b987fd6abe4c67c559e297df25835be94481ab99
Joao <joaopedrocamp554@gmail.com>
2026-02-09 22:35:58 -0300

    testando beecrowd

---
+++ BEECROWD/buscaInternet.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class buscaInternet {
+
+    public static void main(String[] args) {
+
+        int terceiro;
+
+        Scanner dado = new Scanner(System.in);
+
+        terceiro = dado.nextInt();
+
+        int segundo = 2 * terceiro;
+
+        int primeiro = segundo * 2;
+
+        System.out.println(primeiro);
+
+    }
+
+}
