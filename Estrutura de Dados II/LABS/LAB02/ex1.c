6b9ac84cbacc7d6f3d6b69c8b0e15d057c54620b
Joao <joaopedrocamp554@gmail.com>
2026-03-08 09:30:41 -0300

    arrumando pasta

---
+++ Estrutura de Dados II/LABS/LAB02/ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{   
+    char string1[50],string2[50],junta[100];
+
+    fgets(string1,50,stdin);
+    fgets(string2,50,stdin);
+
+    
+    
+
+
+
+
+
+
+    return 0;
+} // end main
